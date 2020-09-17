ca7e458ac0f97e9243a48c59d4db0bd546dea285
webferrol <xurxo@webferrol.com>
2020-09-17 17:11:06 +0200

    persona

--- poo/poo_sesion_3/clases/persona.php
+++ poo/poo_sesion_3/clases/persona.php
@@ -1,10 +1,11 @@
 <?php
 class Persona{
     private string $nombre;
+    private string $sexo; //m --> mujer |  h --> hombre
     private ?string $nif; //Permitimos que el valor del dni sea nulo
     private ?int $edad;  //permitimos que el valor de edad sea nulo
     private float $peso;
-    private float $altura;
+    private float $altura; //altura en metros
 
 
     public function __construct(string $nombre, float $peso, float $altura){
@@ -13,12 +14,14 @@ class Persona{
         $this->altura = $altura;
         $this->edad = null;
         $this->nif = null;
+        $this->sexo = 'm';
     }
 
     public function __toString():string{
         return "<ul>
                     <li>Nombre: <em>$this->nombre</em></li>
-                    <li>NIF: <em>$this->nif €</em></li>
+                    <li>NIF: <em>$this->nif</em></li>
+                    <li>Sexo: <em>$this->sexo</em></li>
                     <li>Edad: <em>$this->edad</em></li>
                     <li>Peso: <em>$this->peso</em></li>
                     <li>Altura: <em>$this->altura</em></li>
